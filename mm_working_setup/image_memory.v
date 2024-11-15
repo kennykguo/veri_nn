34366aa2db6a8ebb89d7dd702a5b941011e8bd92
Kenny Guo <kennykguo@gmail.com>
2024-11-15 17:35:16 -0500

    updates

--- mm_working_setup/image_memory.v
+++ mm_working_setup/image_memory.v
@@ -1,8 +1,8 @@
 module image_memory (
     input wire [15:0] address,
-    output reg [31:0] data_out
+    output reg signed [31:0] data_out
 );
-    reg [31:0] memory [0:3920];  // 32-bit values
+    reg signed [31:0] memory [0:3920];  // 32-bit values
 
     initial begin
         $readmemh("image.mif", memory);
